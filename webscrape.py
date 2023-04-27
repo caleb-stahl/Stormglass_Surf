77dc3595eb928df3087792de559bdec68290c5bd
Caleb Stahl <calebstahl@umail.ucsb.edu>
2023-04-26 23:03:20 -0700

    successful web scrape

--- webscrape.py
+++ webscrape.py
@@ -4,6 +4,9 @@ Webscrape wave height off Surfline
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 # create a ChromeOptions object
@@ -14,6 +17,17 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()),
                           options = chrome_options)
-driver.get("https://www.surfline.com/surf-report/sands/5842041f4e65fad6a7708964")
-print(driver.title)
+driver.get("https://deepswell.com/surf-report/US/Santa-Barbara/Sands-Beach/1547")
+
+
+try:
+    waveheight = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="RenderBodyContainer"]/div/div/div[1]/div[2]/div/div/div[1]/h1'))
+    )
+except:
+    driver.quit()
+
+print(waveheight.text)
+
+
 driver.close()
